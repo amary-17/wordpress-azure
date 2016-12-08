3c2f3daf7f9aff1fdef318db1b8d2a0761a23215
amary-17 <amary-17@mail.ru>
2016-12-08 13:04:36 +0400

    Update index.php

--- index.php
+++ index.php
@@ -50,7 +50,7 @@ catch (PDOException $e) {
     print("Error connecting to SQL Server.");
     die(print_r($e));
 }
-
+//код для вставки регистрационных данных в базу данных
 if(!empty($_POST)) {
 try {
     $name = $_POST['name'];
@@ -71,7 +71,25 @@ catch(Exception $e) {
 }
 echo "<h3>Your're registered!</h3>";
 }
-
+//добавьте код для извлечения данных из базы данных
+$sql_select = "SELECT * FROM registration_tbl";
+$stmt = $conn->query($sql_select);
+$registrants = $stmt->fetchAll(); 
+if(count($registrants) > 0) {
+    echo "<h2>People who are registered:</h2>";
+    echo "<table>";
+    echo "<tr><th>Name</th>";
+    echo "<th>Email</th>";
+    echo "<th>Date</th></tr>";
+    foreach($registrants as $registrant) {
+        echo "<tr><td>".$registrant['name']."</td>";
+        echo "<td>".$registrant['email']."</td>";
+        echo "<td>".$registrant['date']."</td></tr>";
+    }
+    echo "</table>";
+} else {
+    echo "<h3>No one is currently registered.</h3>";
+}
 ?>
 </body>
 </html>
