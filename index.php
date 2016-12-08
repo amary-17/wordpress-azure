8c4951ada140b208a2efc671e343fb38eaf37a50
amary-17 <amary-17@mail.ru>
2016-12-08 08:32:06 +0300

    Update index.php

--- index.php
+++ index.php
@@ -37,7 +37,21 @@ name="submit" value="Submit" />
 </form>
 <?php
 
-
+// DB connection info
+$host = "localhost\sqlexpress";
+$user = "user name";
+$pwd = "password";
+$db = "registration";
+// Connect to database.
+try {
+    $conn = new PDO
+( "sqlsrv:Server= $host ; Database = $db ", $user, $pwd);
+    $conn = new PDO("sqlsrv:server = tcp:registration123.database.windows.net,1433; Database = registration", "amary-17", "{your_password_here}");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch(Exception $e){
+    die(var_dump($e));
+} 
 ?>
 </body>
-</html
+</html>
